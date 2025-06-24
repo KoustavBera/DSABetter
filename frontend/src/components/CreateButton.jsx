9fceae96966b7f02e9df6e9b3b35b42a00b7678f
KoustavBera <thekoustavofficial@gmail.com>
2025-06-24 11:28:27 +0530

    update

--- frontend/src/components/CreateButton.jsx
+++ frontend/src/components/CreateButton.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 
-const CreateButton = () => {
+const CreateButton = ({ className = "" }) => {
   return (
-    <>
+    <div className={className}>
       <button
         id="button1"
-        className="bg-blue-600 text-white px-4 py-2 w-auto mx-2 hover:bg-blue-800 rounded-full"
+        className="bg-blue-600 text-white px-4 py-2 w-auto hover:bg-blue-800 rounded-full"
       >
         Create new question
       </button>
-    </>
+    </div>
   );
 };
 
